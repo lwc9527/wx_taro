f408954a5dd3b28756e9ad17c8ad6033ee447bec
lwc9527 <13795321871@163.com>
2019-03-24 17:31:05 +0800

    tab页及分类

--- src/components/food/food.js
+++ src/components/food/food.js
@@ -1,18 +1,34 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View, Text, Image } from '@tarojs/components'
-import {} from 'taro-ui';
-
+import { AtTabs, AtTabsPane } from 'taro-ui'
+import Cata from './cata'
 class Food extends Component {
 	constructor(){
-		super(...arguments)
+		super(...arguments);
 		this.state = {
-			store: []
-		}
+			current: 0,
+			tabList: [{title: '点菜'},{title: '评价'},{title: '商家'}]
+		};
+	}
+	changeTab (value) {
+	    this.setState({
+	      current: value
+	    })
 	}
 	render () {
-		let {store} = this.state;
-	    return (<View className="food">
-	        
+		let {current,tabList} = this.state;
+		return (<View>
+			<AtTabs current={current} tabList={tabList} onClick={this.changeTab.bind(this)}>
+                <AtTabsPane current={current} index={0} >
+        		    <Cata />
+         		</AtTabsPane>
+         		<AtTabsPane current={current} index={1} >
+        		    222
+         		</AtTabsPane>
+         		<AtTabsPane current={current} index={2} >
+        		    333
+         		</AtTabsPane>
+		    </AtTabs>
 	    </View>)
 	}
 }
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,8 +1,10 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View, Text, Button } from '@tarojs/components'
 import './index.less'
+import "taro-ui/dist/style/components/tabs.scss";
 import  Son from './Son'
 import Head from '../../components/head/head'
+import Food from '../../components/food/food'
 
 export default class Index extends Component {
 
@@ -27,7 +29,8 @@ export default class Index extends Component {
   render () {
     return (
       <View className='index'>
-				<Head/>
+				<Head />
+				<Food />
       </View>
     )
   }
